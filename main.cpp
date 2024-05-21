68e0381bc5c9df1206909e7f6f30ff3115e3955e
Nemesis23 <120707698+Balner123@users.noreply.github.com>
2024-05-21 08:48:23 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,25 @@
 #include <Arduino.h>
 #include <IRremote.hpp>
+
+// Definice použití AVR timeru 1 pro IR přijímač
 #define IR_USE_AVR_TIMER1
-#define IR_RECEIVE_PIN 12
-#define MINUS 8
 
-String nazvy[] = {"RED","GREEN","BLUE"};
+// Definice pinů
+#define IR_RECEIVE_PIN 12  // Pin pro IR přijímač
+#define MINUS 8            // GND pin pro LED diody
 
+// Pole s názvy barev
+String nazvy[] = {"RED", "GREEN", "BLUE"};
+
+// Počet LED diod
 int count = 3;
 
-int leds[] = {9, 10,5};
+// Piny připojené k LED diodám
+int leds[] = {9, 10, 5};
 
+// Pole s IR kódy odpovídajícími číslům 0-9
 unsigned long pole[] = {
-    3910598400,         
+    3910598400,
     4077715200,
     3877175040,
     2707357440,
@@ -20,10 +28,13 @@ unsigned long pole[] = {
     2774204160,
     3175284480,
     2907897600,
-    3041591040};
+    3041591040
+};
 
+// Řetězec pro uchování vstupního čísla z dálkového ovládání
 String stringNum;
 
+// Funkce pro nalezení pozice IR kódu v poli 'pole' a vrácení jako řetězec
 String position(unsigned long code)
 {
     for (int i = 0; i < 10; i++)
@@ -33,25 +44,32 @@ String position(unsigned long code)
             return String(i);
         }
     }
+    return "";
 }
 
 void setup()
 {
-    IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK); // Start the receiver
+    // Inicializace IR přijímače
+    IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
+    // Inicializace sériové komunikace pro ladění
     Serial.begin(9600);
 
+    // Nastavení LED pinů jako výstupy a jejich vypnutí
     for (int i = 0; i < count; i++)
     {
         pinMode(leds[i], OUTPUT);
-        digitalWrite(leds[i],LOW);
+        digitalWrite(leds[i], LOW);
     }
+
+    // Nastavení pinu MINUS jako výstup a jeho vypnutí
     pinMode(MINUS, OUTPUT);
     digitalWrite(MINUS, LOW);
+
+    // Nastavení IR_RECEIVE_PIN jako vstup (není nutné, provádí to IrReceiver.begin)
     pinMode(IR_RECEIVE_PIN, INPUT);
-
-
 }
 
+// Funkce pro vypnutí všech LED diod
 void off_leds()
 {
     for (int i = 0; i < count; i++)
@@ -60,59 +78,74 @@ void off_leds()
     }
 }
 
-
+// Proměnné pro sledování aktuálního stavu a indexu LED
 int d = 0;
 int der = 0;
+
 void loop()
 {
-    if(d==0){
+    // Vytiskne aktuální název barvy, pokud začíná přijímat novou hodnotu
+    if (d == 0)
+    {
         Serial.print(nazvy[der]);
         Serial.print(": ");
         d++;
     }
-    
 
+    // Kontrola, zda byl přijat IR kód
     if (IrReceiver.decode())
     {
-        unsigned long irData = IrReceiver.decodedIRData.decodedRawData; // Print "old" raw data
+        // Získání surových dat IR
+        unsigned long irData = IrReceiver.decodedIRData.decodedRawData;
+
         if (irData > 0)
         {
-            d=0;
+            // Resetování proměnné stavu
+            d = 0;
+            // Definování IR kódu pro tlačítko 'enter'
             unsigned long enter = 4127850240;
-           
+
+            // Kontrola, zda je přijatý IR kód kód 'enter'
             if (irData == enter)
             {
                 if (der < 3)
                 {
+                    // Převod řetězce čísla na celé číslo
                     int intNum = stringNum.toInt();
+                    // Kontrola, zda je číslo v platném rozsahu pro PWM (0-255)
                     if (intNum < 256)
                     {
-
+                        // Nastavení jasu aktuální LED
                         analogWrite(leds[der], intNum);
-
+                        // Vyčištění vstupního řetězce a přechod k další LED
                         stringNum = "";
                         der++;
                     }
                     else
                     {
+                        // Vytištění chybové zprávy, pokud je číslo mimo rozsah
                         Serial.println("Minmorozsah\n");
+                        // Vyčištění vstupního řetězce
                         stringNum = "";
                     }
                 }
                 else
                 {
-                    off_leds(); 
-                    
+                    // Pokud byly nastaveny všechny LED, vypnutí všech LED
+                    off_leds();
+                    // Resetování indexu LED
                     der = 0;
                 }
             }
             else
             {
+                // Přidání přijatého čísla do vstupního řetězce a jeho vytištění
                 stringNum += position(irData);
                 Serial.println(stringNum);
             }
         }
 
+        // Obnovení IR přijímače pro příjem dalšího kódu
         IrReceiver.resume();
     }
 }
